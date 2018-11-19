0a0075ddda4bd5af2788962f785513ce58e4a827
williamperezbeltran <williampbeltranprogramador@gmail.com>
2018-11-19 17:18:04 -0500

    Crea action crear con su respectivo notice

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,18 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "El producto fue publicado con éxito"
+
+    else
+      render 'new'
+      
+    end
   end
 
   def edit
@@ -20,4 +29,9 @@ class ProductsController < ApplicationController
 
   def destroy
   end
+
+  private 
+    def product_params
+      params.require(:product).permit(:name,:url,:description)
+    end
 end
